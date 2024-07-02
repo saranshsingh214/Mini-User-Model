ca10ff52f8f056f29d4ee7e311a5e09770398d94
Saransh Singh <saranshsingh.it2021@nsec.ac.in>
2024-07-03 01:40:03 +0530

    first commit

---
+++ models/user.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+mongoose.connect("mongodb://127.0.0.1:27017/testapp");
+
+const userSchema = mongoose.Schema({
+    image: String,
+    email: String,
+    name: String,
+})
+
+module.exports = mongoose.model('User',userSchema);
